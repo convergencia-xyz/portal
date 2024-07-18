5f0a895e74a32e06ba443d282849896b2171f147
Thiago Bezerra <thiagobezerra.dev@gmail.com>
2024-07-17 23:06:42 -0300

    draft

---
+++ convergencia/__init__.py
@@ -0,0 +1,6 @@
+"""Convergencia: Main package for the Django application.
+
+This package contains the core functionality and configurations for the
+'Convergencia' Django project. It is the entry point for the application,
+defining settings, URLs, WSGI setup, and other application-level configurations.
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Tests package for the Convergencia Django project.
+
+This package contains all the test modules for the 'Convergencia' application.
+It is structured to mirror the main application structure, facilitating
+targeted and comprehensive testing of all components.
+"""
